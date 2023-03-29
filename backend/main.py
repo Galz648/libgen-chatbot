54cb403b77aff4c239a22f6800f59b3411d806ca
Gal Zafar <theycallmefachfach@gmail.com>
2023-03-29 23:10:43 +0300

    added an endpoint to get download links of a pdf given a mirror url
    added custom book return type (unused)

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,27 @@
+from typing import List
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 from libgen_api import LibgenSearch
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+class Book:
+    ID: str
+    Author: str
+    Title: str
+    Publisher: str
+    Year: str
+    Pages: str
+    Language: str
+    Size: str
+    Extension: str
+    Mirror_1: str
+    Mirror_2: str
+    Mirror_3: str
 
+
+app = FastAPI()
+lg = LibgenSearch()
 origins = [
     "http://localhost",
     "http://localhost:8000",
@@ -29,11 +45,18 @@ def read_root(): # only for testing
 
 
 @app.get("/search", response_class=JSONResponse)
-def search_lib_gen(search_string: str):
+def search_lib_gen(search_string: str) : # -> List[Book] - this throws a fastapi error - address
     print(search_string)
-    tf = LibgenSearch()
     title_filters = {"Extension": "pdf"}
-    titles = tf.search_title_filtered(search_string, title_filters)
-    resolve_download_links = [tf.resolve_download_links(item) for item in titles]
-    print(resolve_download_links)
+    titles = lg.search_title_filtered(search_string, title_filters)
     return titles
+
+@app.get("/resolve_download_links", response_class=JSONResponse)
+def resolve_download_links(mirror_link: str) -> dict[str, str]:
+    item: dict[str, str]= {
+        "Mirror_1": mirror_link
+    }
+    download_links = lg.resolve_download_links(item)
+    print(download_links)
+    return download_links
+    
